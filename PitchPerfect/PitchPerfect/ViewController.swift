1feb4da38ea67fccf29ee31f132c8241d17ff0af
Leandro Alves Santos <las@leandros-macbook-air.local>
2018-03-27 01:11:47 -0300

    Layout Primeira Tela

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("recordAudio_Click")
+        recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = !recordButton.isEnabled
+        stopRecordingButton.isEnabled = !stopRecordingButton.isEnabled
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stopAudio_Click")
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = !recordButton.isEnabled
+        stopRecordingButton.isEnabled = !stopRecordingButton.isEnabled
+    }
+    
 }
 
